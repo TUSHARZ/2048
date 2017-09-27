b9dd9fd53505df7c0108796a9e12dae90100961e
TUSHARZ <tusharrrrr97@gmail.com>
2017-09-27 21:50:37 +0530

    made_some_progress

--- static/script.js
+++ static/script.js
@@ -11,17 +11,19 @@ var getrandom = function()
 function startthis(){
 	var v = document.querySelector("#table");
 
-	var randi= Math.floor(Math.random()*(2-0+1)+0);
-		var randj= Math.floor(Math.random()*(2-0+1)+0);
+	var randi= getrandom();
+		var randj= getrandom();
+		var randi2= getrandom();
+		var randj2= getrandom();
 	for(var i=0;i<3;i++){
 		for(var j=0;j<3;j++){
-			 v.rows[i].cells[j].innerHTML = "\n";
+			 v.rows[i].cells[j].innerHTML = "dud";
 			
 		}
 	}
 
-	 var tr=v.rows[getrandom()].cells[getrandom()];
-     var td=v.rows[getrandom()].cells[getrandom()];
+	 var tr=v.rows[randi].cells[randj].innerHTML="2";
+     var td=v.rows[randi2].cells[randj2].innerHTML="4";
 
     
 	document.addEventListener("keydown", function(){
@@ -38,24 +40,37 @@ var print = function(){
 }
 
 var makemove = function(i,j){
+	var v = document.querySelector("#table");
+	var randi=getrandom();
+	var randj=getrandom();
+	v.rows[randi].cells[randj].innerHTML="2";
+
 
 	console.log(event)
 	
-var v = document.querySelector("#table");
+
 
 if(event.keyCode === 40){
 	console.log("in down");
+	console.log("i = ", i);
+	console.log("j = ", j);
+console.log(v.rows[i].cells[j].innerHTML);
+console.log("this, innerHTML = ", this.innerHTML);
+
 	while(i!=2){
-		if(v.rows[i][j].innerHTML != "\n"){
-	if(this.innerHTML == v.rows[i++][j].innerHTML){
-		this.innerHTML = this.innerHTML + v.rows[i][j];
-		i++;
+		console.log("in while");
+		if(v.rows[i].cells[j].innerHTML != "dud"){
+	if(v.rows[i].cells[j].innerHTML == v.rows[i+1].cells[j].innerHTML){
+		console.log("in if");
+		v.rows[i+1].cells[j].innerHTML  = eval(v.rows[i].cells[j].innerHTML  + v.rows[i+1].cells[j].innerHTML);
+		v.rows[i].cells[j].innerHTML="";
 
 	}
 	else{
-		v.rows[i][j]=this.innerHTML;
+		v.rows[i].cells[j]=v.rows[i].cells[j].innerHTML ;
 
 	}}
+	i++;
 
 }
 	
@@ -64,31 +79,34 @@ if(event.keyCode === 40){
 
 if(event.keycode == 38){
 	console.log("in down");
-	while(i!=3){
-		if(this.innerHTML == v.rows[i++][j]){
-		this.innerHTML = this.innerHTML + v.rows[i][j];
+	while(i!=0){
+		if(this.innerHTML == v.rows[i--].cells[j]){
+		this.innerHTML = this.innerHTML + v.rows[i--].cells[j];
+                i--;}
 
-	}
+
+	
 	else{
-		v.rows[3][j]=this.innerHTML;
+		v.rows[i].cells[j]=this.innerHTML;
 
 	}
-
-
-	}
-
-
 }
+
+	}
+
+
+
 
 if(event.keycode == 39){
 	console.log("in down");
-	while(j!=3){
-		if(this.innerHTML == v.rows[i][j++]){
-		this.innerHTML = this.innerHTML + v.rows[i][j];
+	while(j!=2){
+		if(this.innerHTML == v.rows[i].cells[j++]){
+		this.innerHTML = this.innerHTML + v.rows[i].cells[j++];
+		j++;
 
 	}
 	else{
-		v.rows[i][3]=this.innerHTML;
+		v.rows[i].cells[j]=this.innerHTML;
 
 	}
 
@@ -102,12 +120,13 @@ if(event.keycode == 39){
 if(event.keycode == 37){
 	console.log("in down");
 	while(j!=0){
-		if(this.innerHTML == v.rows[i][j--]){
-		this.innerHTML = this.innerHTML + v.rows[i][j];
+		if(this.innerHTML == v.rows[i].cells[j--]){
+		this.innerHTML = this.innerHTML + v.rows[i].cells[j--];
+		j--;
 
 	}
 	else{
-		v.rows[i][0]=this.innerHTML;
+		v.rows[i].cells[j]=this.innerHTML;
 
 	}
 
