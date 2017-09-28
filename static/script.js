8a190c6641fc876d1cf31010397569d3a1224d8e
TUSHARZ <tusharrrrr97@gmail.com>
2017-09-28 18:12:38 +0530

    bug_fix:uncontroll while loop

--- static/script.js
+++ static/script.js
@@ -1,15 +1,9 @@
 var getrandom = function()
 {
-
-
 	return Math.floor(Math.random()*(2-0+1)+0);
-
-
-
-
 }
 var assign = function(){
-	var v = document.querySelector("#table");
+		var v = document.querySelector("#table");
 	var randii=getrandom();
 var randjj=getrandom();
 if(v.rows[randii].cells[randjj].innerHTML==" ")  {
@@ -19,6 +13,41 @@ else{
 	assign();
 
 }
+	for(var i=0 ; i<=2 ; i++){
+		for(var j=0 ; j<=2 ; j++){
+
+			if(v.rows[i].cells[j].innerHTML!=" "){
+                 var mycolor=v.rows[i].cells[j].innerHTML;
+                 switch(mycolor){
+                 case "2": v.rows[i].cells[j].style.backgroundColor="#EEE4DA";
+                       break;
+                 case "4":v.rows[i].cells[j].style.backgroundColor="#EDE0C8"
+                 break;
+                 case "8":v.rows[i].cells[j].style.backgroundColor="#F2B179"
+                 break;
+                 case "16":v.rows[i].cells[j].style.backgroundColor="#F59563"
+                 break;
+                 case "32":v.rows[i].cells[j].style.backgroundColor="#F67C5F"
+                 break;
+                 case "64":v.rows[i].cells[j].style.backgroundColor="#F65E3B"
+                 break
+                 default:
+                 v.rows[i].cells[j].style.backgroundColor="CDC1B4"
+                 
+
+
+
+
+} 
+				
+			}
+			else
+			{
+				v.rows[i].cells[j].style.backgroundColor="#CDC1B4"
+			}
+		}
+	}
+
 	
 
 }
@@ -39,7 +68,9 @@ function startthis(){
 	}
 
 	 var tr=v.rows[randi].cells[randj].innerHTML="2";
+	 v.rows[randi].cells[randj].style.backgroundColor="#ffffff";
      var td=v.rows[randi2].cells[randj2].innerHTML="4";
+     v.rows[randi2].cells[randj2].style.backgroundColor="#ffffff";
 
     
 	document.addEventListener("keydown", function(){
@@ -75,7 +106,7 @@ for(var l=2 ; l>=0 ; l--){  //Just did some work with replacing random variable 
 	for(var k=2 ; k>=0 ; k--){
 		if(v.rows[l].cells[k].innerHTML!=" "){
          
-
+        
 		movedown(l,k,v);
 
 
@@ -160,7 +191,7 @@ var movedown=function(i,j,v){
 		console.log("in if");
 		v.rows[i+1].cells[j].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i+1].cells[j].innerHTML);
 		v.rows[i].cells[j].innerHTML=" ";
-		
+		break;
 		console.log("moved");
 
 	}
@@ -184,7 +215,7 @@ while(i!=0){
 		console.log("in if");
 		v.rows[i-1].cells[j].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i-1].cells[j].innerHTML);
 		v.rows[i].cells[j].innerHTML=" ";
-		
+		break;
 		console.log("moved");
 
 	}
@@ -216,7 +247,7 @@ var moveright = function(i ,j,v){
 		console.log("in if");
 		v.rows[i].cells[j+1].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i].cells[j+1].innerHTML);
 		v.rows[i].cells[j].innerHTML=" ";
-		
+		break;
 		console.log("moved");
 
 	}
@@ -237,7 +268,7 @@ var moveleft = function(i , j , v){
 		console.log("in if");
 		v.rows[i].cells[j-1].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i].cells[j-1].innerHTML);
 		v.rows[i].cells[j].innerHTML=" ";
-		
+		break;
 		console.log("moved");
 
 	}
