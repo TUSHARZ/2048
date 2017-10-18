c84681ce759ed8bbdada1824ea890f599a883dac
Menachem Hornbacher <mendelh1537@gmail.com>
2017-10-17 22:49:58 -0400

    fix formatting and implament game over function

--- static/script.js
+++ static/script.js
@@ -1,63 +1,54 @@
-var getrandom = function()
-{
+var getrandom = function() {
 	return Math.floor(Math.random()*(3-0+1)+0);
 }
 
-
-
-
-
 var assign = function(){
-	
-		var v = document.querySelector("#table");
+	var v = document.querySelector("#table");
 	var randii=getrandom();
-var randjj=getrandom();
-if(v.rows[randii].cells[randjj].innerHTML==" ")  {
-	v.rows[randii].cells[randjj].innerHTML="2";
-}
-else{
-	assign();
-
-}//switching colors
+	var randjj=getrandom();
+	if(v.rows[randii].cells[randjj].innerHTML==" ")  {
+		v.rows[randii].cells[randjj].innerHTML="2";
+	} else if (!isGameOver()){
+		assign();
+	} else {
+		alert("GAME OVER!");
+		showGameOver();
+		return;
+	}
+	//switching colors
 	for(var i=0 ; i<=3 ; i++){
 		for(var j=0 ; j<=3 ; j++){
-			
 			if(v.rows[i].cells[j].innerHTML!=" "){
-
-                 var mycolor=v.rows[i].cells[j].innerHTML;
-                 switch(mycolor){
-                 case "2": v.rows[i].cells[j].style.backgroundColor="#EEE4DA";
-                       break;
-                 case "4":v.rows[i].cells[j].style.backgroundColor="#EDE0C8"
-                 break;
-                 case "8":v.rows[i].cells[j].style.backgroundColor="#F2B179"
-                 break;
-                 case "16":v.rows[i].cells[j].style.backgroundColor="#F59563"
-                 break;
-                 case "32":v.rows[i].cells[j].style.backgroundColor="#F67C5F"
-                 break;
-                 case "64":v.rows[i].cells[j].style.backgroundColor="#F65E3B"
-                 break
-                 default:
-                 v.rows[i].cells[j].style.backgroundColor="#CDC1B4"
-                 
-
-
-
-
-} 
-				
-			}
-			else
-			{
-				v.rows[i].cells[j].style.backgroundColor="#CDC1B4"
+				var mycolor=v.rows[i].cells[j].innerHTML;
+				switch(mycolor){
+					case "2": 
+						v.rows[i].cells[j].style.backgroundColor="#EEE4DA";
+						break;
+					case "4":
+						v.rows[i].cells[j].style.backgroundColor="#EDE0C8";
+						break;
+					case "8":
+						v.rows[i].cells[j].style.backgroundColor="#F2B179";
+						break;
+					case "16":
+						v.rows[i].cells[j].style.backgroundColor="#F59563";
+						break;
+					case "32":
+						v.rows[i].cells[j].style.backgroundColor="#F67C5F";
+						break;
+					case "64":
+						v.rows[i].cells[j].style.backgroundColor="#F65E3B";
+						break;
+					default:
+						v.rows[i].cells[j].style.backgroundColor="#CDC1B4";
+				} 
+			} else {
+				v.rows[i].cells[j].style.backgroundColor="#CDC1B4";
 			}
 		}
 	}
-
-	
-
 }
+
 var newg=function(){
 	console.log("in");
 	window.location.reload(false);
@@ -67,8 +58,6 @@ var seperate = function(i){
 	myrand=getrandom();
 	if(myrand==i){
 		seperate();
-		
-
 	}
 	console.log("myrand = ",myrand );
 	console.log("given = ",i );
@@ -96,35 +85,30 @@ function startthis(){
 	for(var i=0;i<=3;i++){
 		for(var j=0;j<=3;j++){
 			 v.rows[i].cells[j].innerHTML = " ";
-			
 		}
 	}
 
-	 var tr=v.rows[randi].cells[randj].innerHTML="2";
-	 v.rows[randi].cells[randj].style.backgroundColor="#EEE4DA";
-     var td=v.rows[randi2].cells[randj2].innerHTML="4";
-     v.rows[randi2].cells[randj2].style.backgroundColor="#EDE0C8";
+	var tr=v.rows[randi].cells[randj].innerHTML="2";
+	v.rows[randi].cells[randj].style.backgroundColor="#EEE4DA";
+  var td=v.rows[randi2].cells[randj2].innerHTML="4";
+  v.rows[randi2].cells[randj2].style.backgroundColor="#EDE0C8";
 
-    r.addEventListener('click' ,newg);
-    up.addEventListener('click' ,function(){
-    	makemove(randi,randj,"up")
-    });
-     down.addEventListener('click' ,function(){
-    	makemove(randi,randj,"down")
-    });
-      left.addEventListener('click' ,function(){
-    	makemove(randi,randj,"left")
-    });
-       right.addEventListener('click' ,function(){
-    	makemove(randi,randj,"right")
-    });
+	r.addEventListener('click' ,newg);
+	up.addEventListener('click' ,function(){
+		makemove(randi,randj,"up")
+	});
+		down.addEventListener('click' ,function(){
+		makemove(randi,randj,"down")
+	});
+		left.addEventListener('click' ,function(){
+		makemove(randi,randj,"left")
+	});
+			right.addEventListener('click' ,function(){
+		makemove(randi,randj,"right")
+	});
 	document.addEventListener("keydown", function(){
 		makemove(randi,randj);
 	});
-
-
-
-
 }
 
 var print = function(){
@@ -138,227 +122,169 @@ var makemove = function(i,j,k){
 
 	console.log(event)
 	
+	if(event.keyCode === 40 || k==="down"){
+		console.log("in down");
+		console.log("i = ", i);
+		console.log("j = ", j);
+		console.log(v.rows[i].cells[j].innerHTML);
+		console.log("this, innerHTML = ", this.innerHTML);
 
+		for(var l=3 ; l>=0 ; l--){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
+			for(var k=3 ; k>=0 ; k--){
+				if(v.rows[l].cells[k].innerHTML!=" "){
+					movedown(l,k,v,s);
+				}
+			}
+		}
+		assign(v);
+	 } else if(event.keyCode == 38 || k==="up"){
+		console.log("in up");
+		for(var l=0 ; l<=3 ; l++){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
+			for(var k=0 ; k<=3 ; k++){
+				if(v.rows[l].cells[k].innerHTML!=" "){
+					console.log("CALLED IT ");
+					console.log("here l" , l);
+					console.log("here k" , k);
 
-if(event.keyCode === 40 || k==="down"){
-	console.log("in down");
-	console.log("i = ", i);
-	console.log("j = ", j);
-console.log(v.rows[i].cells[j].innerHTML);
-console.log("this, innerHTML = ", this.innerHTML);
-
-for(var l=3 ; l>=0 ; l--){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
-	for(var k=3 ; k>=0 ; k--){
-		if(v.rows[l].cells[k].innerHTML!=" "){
-         
-        
-		movedown(l,k,v,s);
-		
-
-
-	}}
-
-}
-
-
-assign(v);
- }
-
-if(event.keyCode == 38 || k==="up"){
-	console.log("in up");
-for(var l=0 ; l<=3 ; l++){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
-	for(var k=0 ; k<=3 ; k++){
-		if(v.rows[l].cells[k].innerHTML!=" "){
-         console.log("CALLED IT ");
-         console.log("here l" , l);
-         console.log("here k" , k);
-
-		moveup(l,k,v,s);
-
-
-	}}
-
-}
-
-assign(v);
-
-
+					moveup(l,k,v,s);
+				}
+			}
+		}
+		assign(v);
+	} else if(event.keyCode == 39 || k==="right"){
+		console.log("in right");
+		for(var l=3 ; l>=0 ; l--){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
+			for(var k=3 ; k>=0 ; k--){
+				if(v.rows[l].cells[k].innerHTML!=" "){
+					moveright(l,k,v,s);
+				}
+			}
+		}
+		assign(v);
+	} else if(event.keyCode == 37 || k==="left"){
+		console.log("in left");
+		for(var l=0 ; l<=3 ; l++){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
+			for(var k=0 ; k<=3 ; k++){
+				if(v.rows[l].cells[k].innerHTML!=" "){
+					moveleft(l,k,v,s);
+				}
+			}
+		}
+		assign(v);
 	}
-
-
-
-
-if(event.keyCode == 39 || k==="right"){
-	console.log("in right");
-	for(var l=3 ; l>=0 ; l--){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
-	for(var k=3 ; k>=0 ; k--){
-		if(v.rows[l].cells[k].innerHTML!=" "){
-         
-
-		moveright(l,k,v,s);
-
-
-	}}
-
 }
-
-assign(v);
-
-
-}
-
-
-if(event.keyCode == 37 || k==="left"){
-	console.log("in left");
-	for(var l=0 ; l<=3 ; l++){  //Just did some work with replacing random variable on only a blank space and down is almost complete..
-	for(var k=0 ; k<=3 ; k++){
-		if(v.rows[l].cells[k].innerHTML!=" "){
-         
-
-		moveleft(l,k,v,s);
-
-
-	}}
-
-}
-
-assign(v);
-
-
-
-
-}}
 
 var movedown=function(i,j,v,s){
 	while(i!=3){
 		console.log("I" , i);
 		console.log("J", j);
 		
-	if(v.rows[i].cells[j].innerHTML == v.rows[i+1].cells[j].innerHTML){
-		console.log("in if");
-		v.rows[i+1].cells[j].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i+1].cells[j].innerHTML);
-		v.rows[i].cells[j].innerHTML=" ";
-		s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i+1].cells[j].innerHTML) ;
-		break;
-		console.log("moved");
+		if(v.rows[i].cells[j].innerHTML == v.rows[i+1].cells[j].innerHTML){
+			console.log("in if");
+			v.rows[i+1].cells[j].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i+1].cells[j].innerHTML);
+			v.rows[i].cells[j].innerHTML=" ";
+			s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i+1].cells[j].innerHTML) ;
+			break;
+			console.log("moved");
 
+		}
+		else if(v.rows[i+1].cells[j].innerHTML == " "){
+			v.rows[i+1].cells[j].innerHTML=v.rows[i].cells[j].innerHTML ;
+			v.rows[i].cells[j].innerHTML=" ";
+			console.log("replaced");
+		}
+		i++;
 	}
-	else if(v.rows[i+1].cells[j].innerHTML == " "){
-		v.rows[i+1].cells[j].innerHTML=v.rows[i].cells[j].innerHTML ;
-		v.rows[i].cells[j].innerHTML=" ";
-	console.log("replaced");}
-	
-		
-
-	
-	i++;
-}
 }
 
 var moveup = function(i , j, v,s){
-while(i!=0){
+	while(i!=0){
 		console.log("I" , i);
 		
-	if(v.rows[i].cells[j].innerHTML == v.rows[i-1].cells[j].innerHTML){
-		console.log("in if");
-		v.rows[i-1].cells[j].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i-1].cells[j].innerHTML);
-		v.rows[i].cells[j].innerHTML=" ";
-		s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i-1].cells[j].innerHTML);
-		break;
-		console.log("moved");
+		if (v.rows[i].cells[j].innerHTML == v.rows[i-1].cells[j].innerHTML) {
+			console.log("in if");
+			v.rows[i-1].cells[j].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i-1].cells[j].innerHTML);
+			v.rows[i].cells[j].innerHTML=" ";
+			s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i-1].cells[j].innerHTML);
+			break;
+			console.log("moved");
 
-	}
-	else{
-		if(v.rows[i-1].cells[j].innerHTML==" "){
-		v.rows[i-1].cells[j].innerHTML=v.rows[i].cells[j].innerHTML ;
-		v.rows[i].cells[j].innerHTML=" ";}
-		else{
-			v.rows[i].cells[j].innerHTML = v.rows[i].cells[j].innerHTML;
-
+		} else {
+			if (v.rows[i-1].cells[j].innerHTML==" ") {
+				v.rows[i-1].cells[j].innerHTML=v.rows[i].cells[j].innerHTML ;
+				v.rows[i].cells[j].innerHTML=" ";
+			} else {
+				v.rows[i].cells[j].innerHTML = v.rows[i].cells[j].innerHTML;
+			}
+			console.log("replaced");
 		}
-		console.log("replaced");
-
+		i--;
 	}
-	i--;
-}
-
-
-
-
-
 }
 
 var moveright = function(i ,j,v,s){
 	while(j!=3){
 		console.log("I" , i);
 		
-	if(v.rows[i].cells[j].innerHTML == v.rows[i].cells[j+1].innerHTML){
-		console.log("in if");
-		v.rows[i].cells[j+1].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i].cells[j+1].innerHTML);
-		v.rows[i].cells[j].innerHTML=" ";
-		s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i].cells[j+1].innerHTML);
-		break;
-		console.log("moved");
+		if(v.rows[i].cells[j].innerHTML == v.rows[i].cells[j+1].innerHTML){
+			console.log("in if");
+			v.rows[i].cells[j+1].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i].cells[j+1].innerHTML);
+			v.rows[i].cells[j].innerHTML=" ";
+			s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i].cells[j+1].innerHTML);
+			break;
+			console.log("moved");
 
+		} else if(v.rows[i].cells[j+1].innerHTML == " ") {
+			v.rows[i].cells[j+1].innerHTML=v.rows[i].cells[j].innerHTML ;
+			v.rows[i].cells[j].innerHTML=" ";
+			console.log("replaced");
+		}
+		j++;
 	}
-	else if(v.rows[i].cells[j+1].innerHTML == " "){
-		v.rows[i].cells[j+1].innerHTML=v.rows[i].cells[j].innerHTML ;
-		v.rows[i].cells[j].innerHTML=" ";
-	console.log("replaced");}
-	j++;
-}
-
 }
 
 var moveleft = function(i , j , v,s){
 	while(j!=0){
 		console.log("I" , i);
 		
-	if(v.rows[i].cells[j].innerHTML == v.rows[i].cells[j-1].innerHTML){
-		console.log("in if");
-		v.rows[i].cells[j-1].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i].cells[j-1].innerHTML);
-		v.rows[i].cells[j].innerHTML=" ";
-		s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i].cells[j-1].innerHTML);
-		break;
-		console.log("moved");
-
+		if (v.rows[i].cells[j].innerHTML == v.rows[i].cells[j-1].innerHTML) {
+			console.log("in if");
+			v.rows[i].cells[j-1].innerHTML  = parseInt(v.rows[i].cells[j].innerHTML)  + parseInt(v.rows[i].cells[j-1].innerHTML);
+			v.rows[i].cells[j].innerHTML=" ";
+			s.innerHTML=parseInt(s.innerHTML) + Number(v.rows[i].cells[j-1].innerHTML);
+			break;
+			console.log("moved");
+		} else if (v.rows[i].cells[j-1].innerHTML == " ") {
+			v.rows[i].cells[j-1].innerHTML=v.rows[i].cells[j].innerHTML ;
+			v.rows[i].cells[j].innerHTML=" ";
+			console.log("replaced");
+		}
+		j--;
 	}
-	else if(v.rows[i].cells[j-1].innerHTML == " "){
-		v.rows[i].cells[j-1].innerHTML=v.rows[i].cells[j].innerHTML ;
-		v.rows[i].cells[j].innerHTML=" ";
-	console.log("replaced");}
-
-	j--;
 }
 
-
+var isGameOver = function(){
+	// get all the cells
+	var cells = document.querySelectorAll("#table td");
+	// default to game over
+	var gameover = true;
+	// run through all the cells
+	for(var i = 0; i < cells.length; i++){
+		cell = cells[i]; // get the specific cell
+		if (cell.innerText === "") { // check if the cell is blank
+			gameover = false; // if there is an empty cell there is still a move left, so the game is not over
+		}
+	}
+	return gameover;
 }
 
-var gameover=function(){
-	var k = 0;
-	
-	var v = document.querySelector("#table");
-	for(var i=0 ; i<=3 ; i++){
-       for(var j =0 ; j<=3 ; j++){
-       	if(v.rows[i].cells[j].innerHTML!=v.rows[i+1].cells[j].innerHTML && v.rows[i].cells[j].innerHTML!=v.rows[i].cells[j+1].innerHTML
-		&& v.rows[i+1].cells[j].innerHTML!=" " && v.rows[i].cells[j+1].innerHTML!=" " ){
-		k=k+1;
-	    console.log("k = ", k);
-	}
-       }
-
-	}
-	if(k==9){
-		console.log("GAME OVER");
-	}
-	
+var showGameOver = function() {
+	document.getElementById("show-game-over").style.display = "block";
 }
 
+var hideGameOver = function() {
+	document.getElementById("show-game-over").style.display = "none";
+}
 
-
-
-
-
-
-
+// set onload event listenter to the startthis function
 window.addEventListener('load' , startthis);
